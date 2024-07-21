d9d3f956fcc72784b7631d7dfd158221129804ac
gopaldotcom <gopaltomar008@gmail.com>
2024-07-21 15:35:02 +0530

    Create dino game .py

---
+++ dino game .py
@@ -0,0 +1,60 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+import datetime
+import os
+import random
+import threading
+
+import pygame
+
+pygame.init()
+
+# Global Constants
+
+SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1100
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption(" Dino game")
+
+Ico = pygame.image.load("./assets/DinoWallpaper.png")
+pygame.display.set_icon(Ico)
+
+RUNNING = [
+    pygame.image.load(os.path.join("assets/Dino", "DinoRun1.png")),
+    pygame.image.load(os.path.join("assets/Dino", "DinoRun2.png")),
+]
+JUMPING = pygame.image.load(os.path.join("assets/Dino", "DinoJump.png"))
+DUCKING = [
+    pygame.image.load(os.path.join("assets/Dino", "DinoDuck1.png")),
+    pygame.image.load(os.path.join("assets/Dino", "DinoDuck2.png")),
+]
+
+SMALL_CACTUS = [
+    pygame.image.load(os.path.join("assets/Cactus", "SmallCactus1.png")),
+    pygame.image.load(os.path.join("assets/Cactus", "SmallCactus2.png")),
+    pygame.image.load(os.path.join("assets/Cactus", "SmallCactus3.png")),
+]
+LARGE_CACTUS = [
+    pygame.image.load(os.path.join("assets/Cactus", "LargeCactus1.png")),
+    pygame.image.load(os.path.join("assets/Cactus", "LargeCactus2.png")),
+    pygame.image.load(os.path.join("assets/Cactus", "LargeCactus3.png")),
+]
+
+BIRD = [
+    pygame.image.load(os.path.join("assets/Bird", "Bird1.png")),
+    pygame.image.load(os.path.join("assets/Bird", "Bird2.png")),
+]
+
+CLOUD = pygame.image.load(os.path.join("assets/Other", "Cloud.png"))
+
+BG = pygame.image.load(os.path.join("assets/Other", "Track.png"))
+
+FONT_COLOR=(0,0,0)
+
+class Dinosaur:
+
+    X_POS = 80
+    Y_POS = 310
+    Y_POS_DUCK = 340
+    JUMP_VEL = 8.5
